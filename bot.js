4583a031c0bac8470c7b883158c9a84dcf5c70fb
IZIPAY2 <zakharyan311003@gmail.com>
2025-12-18 16:57:54 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,7 +38,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, 'Welcome to IZIPAY Wallet!', {
         reply_markup: {
             inline_keyboard: [
-                [{ text: '👛 Open Wallet', web_app: { url: 'https://izipay2.github.io/izipay-app/' } }]
+                [{ text: ' Open Wallet', web_app: { url: 'https://izipay2.github.io/izipay-app/' } }]
             ]
         }
     }).catch(err => console.error("Ошибка /start:", err.message));
