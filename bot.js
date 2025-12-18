2c9d0bc995c91928e9b822109b729a09fcca2c71
IZIPAY2 <zakharyan311003@gmail.com>
2025-12-18 09:12:59 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Токен от @BotFather
-const token = '8383398356:AAEgAuC_P3yuKy8ohR3up93E19MPaV_lzFU'; 
+const token = '8383398356:AAEgAuC_P3yuKy8ohR3up93E19MPaV_lzFU';
 const bot = new TelegramBot(token, {polling: true});
 
 // Ссылка на твой Mini App
 const webAppUrl = 'https://izipay2.github.io/izipay-app/';
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, 'Добро пожаловать в IZIPAY! Нажмите кнопку ниже, чтобы открыть кошелек.', {
+  bot.sendMessage(msg.chat.id, 'Welcome to IZIPAY! Нажмите кнопку ниже, чтобы открыть кошелек.', {
     reply_markup: {
       inline_keyboard: [
-        [{ text: 'Открыть кошелек', web_app: { url: webAppUrl } }]
+        [{ text: 'Open wallet', web_app: { url: webAppUrl } }]
       ]
     }
   });
